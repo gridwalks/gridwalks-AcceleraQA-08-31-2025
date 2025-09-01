92746c4c6f923fc1f8afcf55e99e10bbcdcc0657
gridwalks <chris@genthq.com>
2025-09-01 18:43:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-//import { useAuth0 } from '@auth0/auth0-react';
-
 import React, { useState, useEffect, useRef } from 'react';
 import { Send, BookOpen, Download, Clock, MessageSquare, LogOut, User } from 'lucide-react';
 
